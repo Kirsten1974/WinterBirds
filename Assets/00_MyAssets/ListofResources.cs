bc8a470afa5d1913ed0094d18c4b33a787d1719d
Sabrina <sabrina.malhi@gmail.com>
2026-03-13 14:54:35 +0200

    FirstRestructuring
    Setting organising

---
+++ Assets/00_MyAssets/ListofResources.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ListofResources : MonoBehaviour
+{
+    /*
+     * This should/could be a readme.txt file or something seperate
+     * 
+     * FPS scripts were kept because they are inspiring(?)
+     * Risto's shared scripts are in his own folder
+     * 
+     */
+}
