8cedbe19733284c9bac8bce45468e6a4b331d209
Sabrina <sabrina.malhi@gmail.com>
2026-04-25 13:37:52 +0300

    Insect
    -Presentation update Until Slide 4
    
    Insect moves around lake

--- Assets/00_MyAssets/Scripts_Us/SpawnManager.cs
+++ Assets/00_MyAssets/Scripts_Us/SpawnManager.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    //landrange
+    int landXRange = 20;
+    int landZRange = 20;
     //nutrition and enemey Settings
     public GameObject[] nutritionPrefabs; //allows us to add nutrition prefabs
     public GameObject[] enemyPrefabs; //allows us to add enemy prefabs
+    //Get info from Weather manager
+    [SerializeField] private WeatherManager manager;
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InvokeRepeating("SpawnEnemy", 2, 10);
+        manager = FindFirstObjectByType<WeatherManager>(); //communicates with WeatherManager
+        InvokeRepeating(nameof(SpawnEnemy), 1, 5);  // previously InvokeRepeating("SpawnEnemy", 2, 10);
+        InvokeRepeating(nameof(SpawnNutrition), 1, 5);  // previously InvokeRepeating("SpawnNutrition", 2, 10);
     }
 
     // Update is called once per frame
@@ -18,7 +26,7 @@ public class SpawnManager : MonoBehaviour
     {
 
         //random range for all nutrition prefabs
-
+        //int nutritionIndex = Random.Range(0, nutritionPrefabs.Length);
         //random range for all nutrition prefabs
         int enemyIndex = Random.Range(0, enemyPrefabs.Length);
 
@@ -34,8 +42,7 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnEnemy()
     {
-        int landXRange = 20;
-        int landZRange = 20;
+
 
         //Randomly generates enemy index and spawn position
         int enemyIndex = Random.Range(0, enemyPrefabs.Length);
@@ -44,5 +51,14 @@ public class SpawnManager : MonoBehaviour
         Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
     }
 
+    void SpawnNutrition()
+    {
+
+        //Randomly generates enemy index and spawn position
+        int nutritionIndex = Random.Range(0, nutritionPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-landXRange, landXRange), 3, Random.Range(-landZRange, landZRange));
+
+        Instantiate(nutritionPrefabs[nutritionIndex], spawnPos, nutritionPrefabs[nutritionIndex].transform.rotation);
+    }
 
 }
